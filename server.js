08f3398d32f60d5f4ccff536e75793f1632df0f5
RobinAnderssonCode <robin_134@hotmail.com>
2016-11-11 16:59:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -55,9 +55,9 @@ var io = require('socket.io')(http);
 
 
 
-    http.listen(3000, function() {
-        console.log('listening on *:3000');
-    });
-    // https.listen(process.env.PORT || 3000);
+   // http.listen(3000, function() {
+     //   console.log('listening on *:3000');
+    //});
+    https.listen(process.env.PORT || 3000);
 
 })();
